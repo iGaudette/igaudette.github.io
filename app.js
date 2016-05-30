138c97429a2a952f2d73b008239673e37cc6dfef
iGaudette <igaudette42@gmail.com>
2016-05-30 18:08:03 -0400

    Initial
    Copy from local files

---
+++ app.js
@@ -0,0 +1,10 @@
+var main = function() {
+	/*$('.dropdown-menu-ijg').hide();*/
+
+	$('.dropdown-toggle-ijg').click(function() {
+		$('.dropdown-menu-ijg').toggle();
+	});
+
+}
+
+$(document).ready(main);
